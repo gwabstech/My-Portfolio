4b0d14cbb63e9b05de0d223d5f2b3e605b56fe34
Gwabstech <Gwabstech@gmail.com>
2026-06-16 17:59:26 +0100

    feat: add router skeleton, SEO title hook, and layout shell

---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import { useDocumentTitle } from '../lib/seo.js'
+
+export default function Home() {
+  useDocumentTitle('')
+  return <main className="pt-24 text-center text-offwhite">Home (under construction)</main>
+}
